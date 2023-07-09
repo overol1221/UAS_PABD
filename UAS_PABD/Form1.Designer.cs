7a4d3c91113c2eb5219ef9e3e86fc807d1e78315
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-07-09 23:42:31 +0700

    Add Form 2 (Form Pendaftaran Mahasiswa)

--- UAS_PABD/Form1.Designer.cs
+++ UAS_PABD/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(154, 208);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 31);
             this.button1.TabIndex = 0;
@@ -47,16 +47,17 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(128, 88);
+            this.label1.Location = new System.Drawing.Point(245, 62);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(121, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Pendaftaran Mahasiswa";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(161, 267);
+            this.button2.Location = new System.Drawing.Point(363, 207);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(67, 32);
             this.button2.TabIndex = 2;
@@ -72,7 +73,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- UAS_PABD/Form2.cs
+++ UAS_PABD/Form2.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace UAS_PABD
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
