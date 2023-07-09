97e0cf9b63efceb3ee248b98d65987dbe580cfc7
overol1221 <fiqiachmad12@gmail.com>
2023-07-10 00:37:45 +0700

    Change font size label

--- UAS_PABD/Form4.Designer.cs
+++ UAS_PABD/Form4.Designer.cs
@@ -34,9 +34,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(336, 76);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.Size = new System.Drawing.Size(144, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Form Admin";
             // 
