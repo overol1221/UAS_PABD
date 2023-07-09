3a17bd6f07d944b1726b952de7b0ff1157bcf7e9
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-07-09 23:50:00 +0700

    Add label Form Data Mahasiswa

--- UAS_PABD/Form2.Designer.cs
+++ UAS_PABD/Form2.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(302, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Form Data Mahasiswa";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
