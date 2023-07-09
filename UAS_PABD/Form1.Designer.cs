4ff02326e7efdfb5e31cf4c0167f45298b681691
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-07-09 23:24:58 +0700

    Add Admin button

--- UAS_PABD/Form1.Designer.cs
+++ UAS_PABD/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(205, 256);
+            this.button1.Location = new System.Drawing.Point(154, 208);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.Size = new System.Drawing.Size(75, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "Mahasiswa";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,20 +47,32 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(171, 108);
+            this.label1.Location = new System.Drawing.Point(128, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.Size = new System.Drawing.Size(121, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Pendaftaran Mahasiswa";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Admin";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -70,6 +84,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
