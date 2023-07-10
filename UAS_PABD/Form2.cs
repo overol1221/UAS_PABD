0b39d4e440078df2c0c5d120024f55ff3315d203
overol1221 <fiqiachmad12@gmail.com>
2023-07-10 22:05:26 +0700

    Add stringConnection from sql database to form 2

--- UAS_PABD/Form2.cs
+++ UAS_PABD/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace UAS_PABD
 {
     public partial class Form2 : Form
     {
+        private string stringConnection = "data source=MAD\\MADDOG12;database=pendaftaran_siswa1;User ID=sa; Password=fiqi1234";
+        private SqlConnection koneksi;
         public Form2()
         {
             InitializeComponent();
@@ -22,6 +25,11 @@ namespace UAS_PABD
             Form1 f1 = new Form1();
             f1.Show();
             this.Hide();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
