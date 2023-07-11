86d993bcbf45dfed664e98acdcaf8bcc59bd5598
overol1221 <fiqiachmad12@gmail.com>
2023-07-11 21:04:46 +0700

    Change BackColor Form Admin

--- UAS_PABD/Form3.Designer.cs
+++ UAS_PABD/Form3.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label1);
             this.Name = "Form3";
