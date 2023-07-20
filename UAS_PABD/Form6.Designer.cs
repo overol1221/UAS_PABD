b4966ffce69ae79cdc4bf433eb88d39a7a567c95
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-07-20 17:16:04 +0700

    Menambahkan Blob Data dalam Form 6

--- UAS_PABD/Form6.Designer.cs
+++ UAS_PABD/Form6.Designer.cs
@@ -37,13 +37,19 @@
             this.txtNik = new System.Windows.Forms.TextBox();
             this.txtNama = new System.Windows.Forms.TextBox();
             this.txtPkrjn = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(271, 49);
+            this.label1.Location = new System.Drawing.Point(277, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(249, 24);
             this.label1.TabIndex = 0;
@@ -53,7 +59,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(62, 136);
+            this.label2.Location = new System.Drawing.Point(36, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 16);
             this.label2.TabIndex = 1;
@@ -63,7 +69,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(62, 169);
+            this.label3.Location = new System.Drawing.Point(36, 156);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 16);
             this.label3.TabIndex = 2;
@@ -73,7 +79,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(62, 201);
+            this.label4.Location = new System.Drawing.Point(36, 188);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(69, 16);
             this.label4.TabIndex = 3;
@@ -81,7 +87,7 @@
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(184, 249);
+            this.btnBack.Location = new System.Drawing.Point(158, 236);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(75, 23);
             this.btnBack.TabIndex = 4;
@@ -91,7 +97,7 @@
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(265, 249);
+            this.btnSubmit.Location = new System.Drawing.Point(239, 236);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(75, 23);
             this.btnSubmit.TabIndex = 5;
@@ -100,24 +106,69 @@
             // 
             // txtNik
             // 
-            this.txtNik.Location = new System.Drawing.Point(184, 132);
+            this.txtNik.Location = new System.Drawing.Point(158, 119);
             this.txtNik.Name = "txtNik";
             this.txtNik.Size = new System.Drawing.Size(195, 20);
             this.txtNik.TabIndex = 6;
             // 
             // txtNama
             // 
-            this.txtNama.Location = new System.Drawing.Point(184, 166);
+            this.txtNama.Location = new System.Drawing.Point(158, 153);
             this.txtNama.Name = "txtNama";
             this.txtNama.Size = new System.Drawing.Size(195, 20);
             this.txtNama.TabIndex = 7;
             // 
             // txtPkrjn
             // 
-            this.txtPkrjn.Location = new System.Drawing.Point(184, 200);
+            this.txtPkrjn.Location = new System.Drawing.Point(158, 187);
             this.txtPkrjn.Name = "txtPkrjn";
             this.txtPkrjn.Size = new System.Drawing.Size(195, 20);
             this.txtPkrjn.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(466, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(232, 197);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(544, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(505, 352);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(505, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(586, 398);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Display";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form6
             // 
@@ -125,6 +176,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtPkrjn);
             this.Controls.Add(this.txtNama);
             this.Controls.Add(this.txtNik);
@@ -137,6 +193,7 @@
             this.Name = "Form6";
             this.Text = "Form6";
             this.Load += new System.EventHandler(this.Form6_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,5 +210,10 @@
         private System.Windows.Forms.TextBox txtNik;
         private System.Windows.Forms.TextBox txtNama;
         private System.Windows.Forms.TextBox txtPkrjn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- UAS_PABD/Form6.cs
+++ UAS_PABD/Form6.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace UAS_PABD
 {
     public partial class Form6 : Form
     {
+        Image CurImage;
+        string curFileName;
+        string connectionString = "data source=ALDAYANDAY\\ALDAYANDAY; database=BLOB; MultipleActiveResultSets=True; User ID=Sa; Password=Unkown007";
+        string savedImageName = "";
         public Form6()
         {
             InitializeComponent();
@@ -27,6 +34,99 @@ namespace UAS_PABD
             Form1 f1 = new Form1();
             f1.Show();
             this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            if (openDlg.ShowDialog() == DialogResult.OK)
+            {
+                curFileName = openDlg.FileName;
+                textBox1.Text = curFileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                FileStream file = new FileStream(curFileName, FileMode.OpenOrCreate, FileAccess.Read);
+                byte[] rawdata = new byte[file.Length];
+                file.Read(rawdata, 0, System.Convert.ToInt32(file.Length));
+                file.Close();
+
+                string sql = "SELECT * FROM Mahasiswa";
+                SqlConnection connection = new SqlConnection();
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                SqlCommandBuilder cmdBuilder = new SqlCommandBuilder(adapter);
+                DataSet ds = new DataSet("Mahasiswa");
+                adapter.Fill(ds, "Mahasiswa");
+
+                DataRow row = ds.Tables["Mahasiswa"].NewRow();
+                row["Nim"] = 1;
+                row["Nama"] = "SQL";
+                row["Foto"] = rawdata;
+                ds.Tables["Mahasiswa"].Rows.Add(row);
+                adapter.Update(ds, "Mahasiswa");
+                connection.Close();
+
+                MessageBox.Show("Image saved");
+            }
+            else
+            {
+                MessageBox.Show("Click the Browse button to select an image");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                string sql = "SELECT Foto FROM Mahasiswa WHERE Nim='1'";
+                SqlConnection connection = new SqlConnection();
+                connection.ConnectionString = connectionString;
+                connection.Open();
+                FileStream file;
+                BinaryWriter bw;
+                int bufferSize = 100;
+                byte[] outbyte = new byte[bufferSize];
+                long retval;
+                long startIndex = 0;
+                savedImageName = textBox1.Text;
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataReader myReader = command.ExecuteReader(CommandBehavior.SequentialAccess);
+                while (myReader.Read())
+                {
+                    file = new FileStream(savedImageName, FileMode.OpenOrCreate, FileAccess.Write);
+                    bw = new BinaryWriter(file);
+                    startIndex = 0;
+                    retval = myReader.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                    while (retval == bufferSize)
+                    {
+                        bw.Write(outbyte);
+                        bw.Flush();
+                        startIndex += bufferSize;
+                        retval = myReader.GetBytes(0, startIndex, outbyte, 0, bufferSize);
+                    }
+                    bw.Write(outbyte, 0, (int)retval);
+                    bw.Flush();
+                    bw.Close();
+                    file.Close();
+                }
+                connection.Close();
+
+                CurImage = Image.FromFile(savedImageName);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // Atur properti SizeMode menjadi Zoom
+                pictureBox1.Image = CurImage;
+                pictureBox1.Invalidate();
+                connection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Upload the image first");
+            }
         }
     }
 }
